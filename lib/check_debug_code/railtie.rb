b317541f6acbc18866e1a8d18dbe83538c09f423
hataken <bb52119638@gmail.com>
2024-06-20 00:39:05 +0900

    fixed

--- lib/check_debug_code/railtie.rb
+++ lib/check_debug_code/railtie.rb
@@ -2,9 +2,6 @@ module CheckDebugCode
   class Railtie < ::Rails::Railtie
     initializer "check_debug_code.configure_rails_initialization" do |app|
       app.middleware.use CheckDebugCode::Middleware
-
-      Rails.logger.info "ほげほげ"
-
     end
   end
 end
