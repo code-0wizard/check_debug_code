a93fb064d7b1eed5c768715fdf152e28cf5a5759
hataken <bb52119638@gmail.com>
2024-06-19 18:49:11 +0900

    fixed

--- lib/generators/check_debug_code/install_generator.rb
+++ lib/generators/check_debug_code/install_generator.rb
@@ -2,21 +2,40 @@ require 'rails/generators'
 
 module CheckDebugCode
   class InstallGenerator < Rails::Generators::Base
-    # def enable_in_development
-    #   environment(nil, env: 'development') do
-    #     <<-FILE
-    #       config.after_initialize do
-    #         Bullet.enable        = true
-    #         Bullet.alert         = true
-    #         Bullet.bullet_logger = true
-    #         Bullet.console       = true
-    #         Bullet.rails_logger  = true
-    #         Bullet.add_footer    = true
-    #       end
-    #     FILE
-    #   end
-    #   say 'Enabled check_debug_code in config/environments/development.rb'
-    # end
-    
+    def enable_in_development
+      def enable_in_development
+        environment(nil, env: 'development') do
+          <<~FILE
+            config.after_initialize do
+              Bullet.enable        = true
+              Bullet.alert         = true
+              Bullet.bullet_logger = true
+              Bullet.console       = true
+              Bullet.rails_logger  = true
+              Bullet.add_footer    = true
+            end
+
+          FILE
+        end
+
+        say 'Enabled bullet in config/environments/development.rb'
+      end
+
+      def enable_in_test
+        return unless yes?('Would you like to enable bullet in test environment? (y/n)')
+
+        environment(nil, env: 'test') do
+          <<~FILE
+            config.after_initialize do
+              Bullet.enable        = true
+              Bullet.bullet_logger = true
+              Bullet.raise         = true # raise an error if n+1 query occurs
+            end
+
+          FILE
+        end
+
+        say 'Enabled bullet in config/environments/test.rb'
+      end
   end
 end
