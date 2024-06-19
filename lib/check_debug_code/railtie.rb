d4f68510f8bdb21c43384b04ccdf9e743807eda7
hataken <bb52119638@gmail.com>
2024-06-19 17:35:38 +0900

    fixed

--- lib/check_debug_code/railtie.rb
+++ lib/check_debug_code/railtie.rb
@@ -3,6 +3,8 @@ module CheckDebugCode
     initializer "check_debug_code.configure_rails_initialization" do |app|
       app.middleware.use CheckDebugCode::Middleware
 
+      Rails.logger.info "ホゲホゲ"
+      
       config.after_initialize do
         unless Rails.env.production?
           CheckDebugCode::InstallGenerator.start
