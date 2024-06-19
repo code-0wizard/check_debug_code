5524f6f7ce29dd873f48d000d6e50f0cb03bb460
hataken <bb52119638@gmail.com>
2024-06-19 18:07:43 +0900

    fixed

--- lib/check_debug_code/railtie.rb
+++ lib/check_debug_code/railtie.rb
@@ -3,7 +3,18 @@ module CheckDebugCode
     initializer "check_debug_code.configure_rails_initialization" do |app|
       app.middleware.use CheckDebugCode::Middleware
 
-      Rails.logger.info "ほげほげ"
+      environment(nil, env: 'development') do
+        <<-FILE
+          config.after_initialize do
+            Bullet.enable        = true
+            Bullet.alert         = true
+            Bullet.bullet_logger = true
+            Bullet.console       = true
+            Bullet.rails_logger  = true
+            Bullet.add_footer    = true
+          end
+        FILE
+      end
 
     end
   end
--- lib/generators/check_debug_code/install_generator.rb
+++ lib/generators/check_debug_code/install_generator.rb
@@ -2,20 +2,21 @@ require 'rails/generators'
 
 module CheckDebugCode
   class InstallGenerator < Rails::Generators::Base
-    def enable_in_development
-      environment(nil, env: 'development') do
-        <<-FILE
-          config.after_initialize do
-            Bullet.enable        = true
-            Bullet.alert         = true
-            Bullet.bullet_logger = true
-            Bullet.console       = true
-            Bullet.rails_logger  = true
-            Bullet.add_footer    = true
-          end
-        FILE
-      end
-      say 'Enabled check_debug_code in config/environments/development.rb'
-    end
+    # def enable_in_development
+    #   environment(nil, env: 'development') do
+    #     <<-FILE
+    #       config.after_initialize do
+    #         Bullet.enable        = true
+    #         Bullet.alert         = true
+    #         Bullet.bullet_logger = true
+    #         Bullet.console       = true
+    #         Bullet.rails_logger  = true
+    #         Bullet.add_footer    = true
+    #       end
+    #     FILE
+    #   end
+    #   say 'Enabled check_debug_code in config/environments/development.rb'
+    # end
+    
   end
 end
