e7812f583e6c5e6f9964895bdabf5d569a244bb8
hataken <bb52119638@gmail.com>
2024-06-19 18:31:19 +0900

    fixed

--- lib/check_debug_code/railtie.rb
+++ lib/check_debug_code/railtie.rb
@@ -3,18 +3,7 @@ module CheckDebugCode
     initializer "check_debug_code.configure_rails_initialization" do |app|
       app.middleware.use CheckDebugCode::Middleware
 
-      environment(nil, env: 'development') do
-        <<-FILE
-          config.after_initialize do
-            Bullet.enable        = true
-            Bullet.alert         = true
-            Bullet.bullet_logger = true
-            Bullet.console       = true
-            Bullet.rails_logger  = true
-            Bullet.add_footer    = true
-          end
-        FILE
-      end
+      Rails.logger.info "ほげほげ"
 
     end
   end
